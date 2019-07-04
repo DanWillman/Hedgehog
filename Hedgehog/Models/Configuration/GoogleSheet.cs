144499ca805537a0f66b90f1e44f54dcbd23eed8
Dan Willman <RikerCabra@gmail.com>
2019-07-04 10:11:49 -0500

    Add config models for secrets

---
+++ Hedgehog/Models/Configuration/GoogleSheet.cs
@@ -0,0 +1,13 @@
+namespace Hedgehog.Models.Configuration
+{
+    /// <summary>
+    /// Configuration model for GoogleSheet secrets
+    /// </summary>
+    public class GoogleSheet
+    {
+        /// <summary>
+        /// Target sheet ID
+        /// </summary>
+        public string SheetId { get; set; }
+    }
+}
