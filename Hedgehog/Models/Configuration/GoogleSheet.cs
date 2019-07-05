f5ad5db82e32ab4e8b6e77ba0e2bd2303e66229b
Dan Willman <RikerCabra@gmail.com>
2019-07-05 14:47:08 -0500

    Add missing field

--- Hedgehog/Models/Configuration/GoogleSheet.cs
+++ Hedgehog/Models/Configuration/GoogleSheet.cs
@@ -9,5 +9,10 @@ namespace Hedgehog.Models.Configuration
         /// Target sheet ID
         /// </summary>
         public string SheetId { get; set; }
+
+        /// <summary>
+        /// Target Sheet name for data destination
+        /// </summary>
+        public string SheetName { get; set; }
     }
 }
