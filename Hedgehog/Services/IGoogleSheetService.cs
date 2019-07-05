8a50d02858400e44a8e19cfd21e2e9955a92295d
Dan Willman <RikerCabra@gmail.com>
2019-07-05 14:43:26 -0500

    Giver service Interface

--- Hedgehog/Services/IGoogleSheetService.cs
+++ Hedgehog/Services/IGoogleSheetService.cs
@@ -1,6 +1,11 @@
-﻿namespace Hedgehog.Services
+﻿using Hedgehog.Models;
+
+namespace Hedgehog.Services
 {
     public interface IGoogleSheetService
     {
+        void CreateEntry(TestResult dataPoint);
+
+        void ReadEntries();
     }
 }
