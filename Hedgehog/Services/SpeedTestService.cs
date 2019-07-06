72123673107ef416480dbcca6ed6189e75d810f3
Dan Willman <RikerCabra@gmail.com>
2019-07-06 09:45:53 -0500

    "Randomize" user agent
    Closes #4

--- Hedgehog/Services/SpeedTestService.cs
+++ Hedgehog/Services/SpeedTestService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using Hedgehog.Models;
@@ -11,10 +12,12 @@ namespace Hedgehog.Services
     public class SpeedTestService : ISpeedTestService
     {
         private readonly IConfiguration config;
+        private Random rand;
 
         public SpeedTestService(IConfiguration config)
         {
             this.config = config;
+            rand = new Random();
         }
 
         public TestResult GetData()
@@ -27,7 +30,7 @@ namespace Hedgehog.Services
 
             ChromeOptions options = new ChromeOptions();
             options.AddArguments("headless", "no-sandbox");
-            //options.AddArgument("--user-agent=Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5355d Safari/8536.25");
+            options.AddArgument($"user-agent={GetRandomUserAgent()}");
             options.BinaryLocation = $"{config["ChromeDriver:ChromeBinaryLocation"]}";
             ChromeDriverService cds = ChromeDriverService.CreateDefaultService($"{config["ChromeDriver:SeleniumLocation"]}", config["ChromeDriver:SeleniumDriverName"]);
 
@@ -48,21 +51,20 @@ namespace Hedgehog.Services
                 var downResults = driver.FindElements(By.CssSelector("[class='result-data-large number result-data-value download-speed']"));
                 if (downResults.Count > 0)
                 {
-                    Console.WriteLine($"Found {downResults.Count} results for download speed.");
                     foreach (var down in downResults)
                         downSpeed += $"{down.Text}";
 
                     Console.WriteLine($"Download speed(s) {downSpeed}Mbps");
 
                     var upResults = driver.FindElements(By.CssSelector("[class='result-data-large number result-data-value upload-speed']"));
-                    Console.WriteLine($"Found {upResults.Count} results for upload speed.");
+                    
                     foreach (var up in upResults)
                         upSpeed += $"{up.Text}";
 
                     Console.WriteLine($"Upload speed(s) {upSpeed}Mbps");
 
                     var pingResults = driver.FindElements(By.CssSelector("[class='result-data-large number result-data-value ping-speed']"));
-                    Console.WriteLine($"Found {pingResults.Count} results for ping");
+                    
                     foreach (var ping in pingResults)
                         latency += $"{ping.Text}";
 
@@ -85,6 +87,21 @@ namespace Hedgehog.Services
                 Latency = latency,
                 ClientName = Environment.MachineName
             };
+        }
+
+        private string GetRandomUserAgent()
+        {
+            List<string> userAgents = new List<string>()
+            {
+                "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17",
+            };
+            int randomIndex = rand.Next(userAgents.Count - 1);
+
+            Console.WriteLine($"Using user agent - {userAgents[randomIndex]}");
+
+            return userAgents[randomIndex];
         }
     }
 }
